7c9b88db162ffb2c5420cd8acf683402f1f32aa8
yuzuru <44111695+yamaiYuzuru@users.noreply.github.com>
2025-08-08 08:39:39 +0200

    Create de.ts
    Feat: Added german language

---
+++ src/i18n/languages/de.ts
@@ -0,0 +1,38 @@
+import Key from "../i18nKey";
+import type { Translation } from "../translation";
+
+export const de: Translation = {
+	[Key.home]: "Hauptseite",
+	[Key.about]: "Über",
+	[Key.archive]: "Archive",
+	[Key.search]: "Suche",
+
+	[Key.tags]: "Tags",
+	[Key.categories]: "Kategorien",
+	[Key.recentPosts]: "Neuste Beiträge",
+
+	[Key.comments]: "Kommentare",
+
+	[Key.untitled]: "Ohne Titel",
+	[Key.uncategorized]: "Nicht Kategorisiert",
+	[Key.noTags]: "Keine Tags",
+
+	[Key.wordCount]: "Wort",
+	[Key.wordsCount]: "Wörter",
+	[Key.minuteCount]: "Minute",
+	[Key.minutesCount]: "Minuten",
+	[Key.postCount]: "Beitrag",
+	[Key.postsCount]: "Beiträge",
+
+	[Key.themeColor]: "Thema Farbe",
+
+	[Key.lightMode]: "Hell",
+	[Key.darkMode]: "Dunkel",
+	[Key.systemMode]: "System",
+
+	[Key.more]: "Mehr",
+
+	[Key.author]: "Ersteller",
+	[Key.publishedAt]: "Veröffentlicht am",
+	[Key.license]: "Lizenz",
+};
